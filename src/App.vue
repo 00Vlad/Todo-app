bcb8d97b443c6fc74a4fbbee3e1ab7bb8e61c20c
Vlad <57154809+Vlad043@users.noreply.github.com>
2020-12-19 19:29:21 +0100

    Final commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,105 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span>Todo Application</span>
+    <hr>
+    <AddTodo
+      @add-todo='addTodo'
+    />
+    <select v-model="filter">
+      <option value="all">All</option>
+      <option value="completed">Completed</option>
+      <option value="not-completed">Not Completed</option>
+    </select>
+    <hr>
+    <Loader v-if="loading"/>
+    <TodoList 
+      v-else-if="filteredTodos.length"
+      :todos='filteredTodos'
+      @remove-todo='removeTodo'
+    />
+    <p v-else>No todos</p>
   </div>
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from '@/components/TodoList.vue'
+import AddTodo from '@/components/AddTodo.vue'
+import Loader from '@/components/Loader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList, AddTodo, Loader
+  },
+
+  data() {
+    return {
+      todos: [],
+      loading: true,
+      filter: 'not-completed'
+    }
+  },
+
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(json => {
+        setTimeout(() => {
+          this.todos = json
+          this.loading = false
+        }, 500)
+      })
+  },
+  computed: {
+    filteredTodos() {
+      switch (this.filter) {
+        case 'all':
+          return this.todos
+
+        case 'completed':
+          return this.todos.filter(el => el.completed)
+        
+        case 'not-completed':
+          return this.todos.filter(el => !el.completed)
+      }
+      return 'all'
+    }
+  },
+
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter((el) =>el.id !== id)
+    },
+    addTodo(newTodo) {
+      this.todos.push(newTodo)
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+
+<style scoped>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+    text-align: center;
 }
+  span {
+    display: block;
+    font-size: 1.4rem;
+    font-weight: 700;
+    margin-bottom: 1.5rem;
+  }
+  select {
+    margin-top: .6rem;
+  }
+  hr {
+    margin-bottom: 1.5rem;
+  }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,90 @@
+<template>
+		<li>
+			<span :class="{done: todo.completed}">
+				<input type="checkbox" @change="todo.completed = !todo.completed">
+				<strong>{{ index + 1 }}</strong>
+				{{ todo.title | uppercase}}
+			</span>
+			<button class="rm" @click="$emit('remove-todo', todo.id)">
+				&times;
+			</button>
+		</li>
+</template>
+
+
+
+<script>
+export default {
+	props: {
+		todo: {
+			type: Object,
+			required: true
+    },
+    index: Number
+	},
+	computed: {
+		
+	},
+	filters: {
+		uppercase(value) {
+			return value[0].toUpperCase() + value.slice(1)
+		}
+	}
+}
+</script>
+
+
+
+<style scoped>
+	li {
+		display: flex;
+		justify-content: space-between;
+
+		padding: .5rem 2rem;
+		margin-bottom: 1rem;
+
+		border: 1px solid #ccc;
+    cursor: pointer;
+    transition: all .2s ease;
+	}
+  li:hover {
+    background: rgb(230, 230, 230);
+    padding: .8rem 2.2rem;
+    font-size: 1.1rem;
+  }
+  li:hover input {
+    visibility: visible;
+  }
+	input {
+    visibility: hidden;
+		margin-right: 1rem;
+	}
+  span {
+    padding-top: .2em;
+  }
+	.rm {
+		background: red;
+		color: white;
+    border: 2px solid red;
+
+		font-size: 1.2rem;
+		font-weight: bold;
+
+    border-radius: 50%;
+    outline: none;
+		box-shadow: 0 1px 3px rgb(99, 99, 99);
+    cursor: pointer;
+    transition: all .3s ease;
+	}
+  .rm:hover {
+    background: white;
+    color: red;
+  }
+  .rm:active {
+    box-shadow: 0 0 10px rgb(99, 99, 99);
+  }
+	.done {
+		color: #ccc;
+		text-decoration: line-through;
+	}
+</style>
